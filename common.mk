fd344a445247893a5c080d88287eb2e1c2be2a1d
nguyenhung9xdev <nguyenhung9xdev2022@gmail.com>
2023-05-27 14:12:28 +0700

    sm7325-common: build hal soundtrigger@2.3

--- common.mk
+++ common.mk
@@ -76,7 +76,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl.samsung-sm7325 \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audioadsprpcd \
     audio.r_submix.default \
     audio.usb.default \
